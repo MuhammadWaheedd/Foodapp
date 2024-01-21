e0d86d637437d4af3a432a907c2a875cbf6f6a99
Muhammad Waheed <156705548+MuhammadWaheedd@users.noreply.github.com>
2024-01-21 16:04:49 +0500

    Add files via upload

---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,24 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "E:\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "C:\\Users\\WAHEED\\StudioProjects\\my_Food_App" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=E:\\flutter"
+  "PROJECT_DIR=C:\\Users\\WAHEED\\StudioProjects\\my_Food_App"
+  "FLUTTER_ROOT=E:\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=C:\\Users\\WAHEED\\StudioProjects\\my_Food_App\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=C:\\Users\\WAHEED\\StudioProjects\\my_Food_App"
+  "FLUTTER_TARGET=C:\\Users\\WAHEED\\StudioProjects\\my_Food_App\\lib\\main.dart"
+  "DART_DEFINES=Zmx1dHRlci5pbnNwZWN0b3Iuc3RydWN0dXJlZEVycm9ycz10cnVl,RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ==,RkxVVFRFUl9XRUJfQ0FOVkFTS0lUX1VSTD1odHRwczovL3d3dy5nc3RhdGljLmNvbS9mbHV0dGVyLWNhbnZhc2tpdC9hNzk0Y2YyNjgxYzZjOWZlN2IyNjBlMGU4NGRlOTYyOThkYzljMThiLw=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=C:\\Users\\WAHEED\\StudioProjects\\my_Food_App\\.dart_tool\\package_config.json"
+)
